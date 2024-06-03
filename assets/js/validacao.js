a9933c8d45b0e234658c92d274280d82c34ff978
Matheus Freitas <matheusrlcm@gmail.com>
2024-06-02 23:37:11 -0300

    Inclusão lógica de acesso pelo apí e redirecionamento

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -1,14 +1,25 @@
-function validateLogin(event) {
+async function validateLogin(event) {
     event.preventDefault(); // Previne o envio do formulário
     
-    var login = document.getElementById('login').value;
+    var login = document.getElementById('login').value.toLowerCase();
     var password = document.getElementById('password').value;
-    
-    if (login === 'admin' && password === '102030') {
+
+    // Puxando usuarios da api do professor
+    let url = 'https://back-login.vercel.app/usuarios';
+    let response = await fetch(url);
+    let userData = await response.json();
+
+    let authenticated = userData.some(user => {
+        return (login === user.email.toLowerCase() || login == user.nome.toLowerCase()) && password === user.senha;
+    });
+
+    if (authenticated) {
+        localStorage.setItem('authenticated', 'true');
         window.location.href = '../admin/index.html'; // Redireciona para o diretório cadastro/
     } else {
         alert('Login ou senha incorretos!');
         document.querySelector('#login').value = '';
         document.querySelector('#password').value = '';
     }
+    // https://back-login.vercel.app/usuarios
 }
